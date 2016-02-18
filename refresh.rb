57da5e840490b0700945d79f1c393bef7e1a2984
Colin Osborn <Colin.Osborn@gmail.com>
2016-02-17 22:49:39 -0700

    mas

--- refresh.rb
+++ refresh.rb
@@ -18,7 +18,80 @@ end
 puts movie_listing("snatch", 9)
 puts movie_listing("goldfinger", 7)
 
+################################################################################
 
+class Movie
+  attr_reader :rank
+              # :title -- no longer need this here, because attr_accessor does both
+  # Remember, this is just like writing a method
+  attr_accessor :title
+
+  def initialize(title, rank)
+    @title = title.capitalize
+    @rank = rank
+    # @rank is an instance variable and, rank is a local variable.
+    puts "Created a movie object with title #{@title} and a rank of #{@rank}"
+    # Think of this puts statement whenever you use initialize
+  end
+
+  def normalized_rank
+    @rank / 5
+    # inclination is to use this on the initialize method, however, once again, it can just
+    # as easily be called below, as seen on line -- 78
+  end
+
+  def thumbs_up
+    @rank += 1
+  end
+
+  def thumbs_down
+    @rank -= 1
+  end
+
+  # def title=(new_title)
+  #   @title = new_title
+  # end -- you no longer need this, because you added the attr_writer
+
+  # def title
+  #   @title
+  # end --- you no longer need this, because you added the attr_reader
+
+  def to_s
+    # puts "#{@title} has a rank of #{@rank}"
+    puts "#{@title} has a rank of #{normalized_rank}"
+    # remember, this is a method call right above
+  end
+end
+
+movie1 = Movie.new("Rush", 10)
+movie1.thumbs_up
+puts movie1
+# .listing, is the instance method here!
+movie2 = Movie.new("Snatch", 8)
+movie2.thumbs_down
+puts movie2
+movie3 = Movie.new("Die Hard", 9)
+puts movie3
+
+movie2.title = "Lock Stock, Two Smoking Barrels"
+puts movie2
+puts movie1.normalized_rank
+
+movies = [movie1, movie2, movie3]
+
+movies.each do |m|
+  m.thumbs_up
+  puts movies
+end
+
+
+chasis = %w(Mclaren Renault Ferrari)
+puts chasis
+#  puts chasis in pry!
+# beta = %w(_ , .)
+#When you call new, it sets up some memory for that object,
+# but it doesn't initialize any of it's state. Instead, New will pass in the
+# the parameters, "Rush", 10
 
 # to run a method, we call it by name
 # wanna call it with someone's name? add an arguement or Parameter to the method
@@ -34,3 +107,8 @@ puts movie_listing("goldfinger", 7)
 
  # State + behaviour = Object
  #
+
+# Tell, don't ask
+#  Tell the object what to do, we tell an object, what to do by calling a method
+
+# set up state -- by setting up instance variables -- look at the photos!
